57eb40a9909311b98d6d1e212bb608264aa76301
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2023-11-13 14:29:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 import streamlit as st
+from PIL import Image
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
 from langchain.llms import Replicate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import os
 from dotenv import load_dotenv
 import tempfile
 
-# Load environment variables
+
 load_dotenv()
+
 
 def initialize_session_state():
     if 'history' not in st.session_state:
         st.session_state['history'] = []
+
     if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! This is a contract evaluation tool"]
+        st.session_state['generated'] = ["Hello! This is a Contracts Evaluation Tool"]
+        
+
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hey! 👋"]
 
@@ -34,11 +42,11 @@ def display_chat_history(chain):
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Question:", placeholder="Ask about your Documents", key='input')
+            user_input = st.text_input("Question:", placeholder="Ask me about your Documents", key='input')
             submit_button = st.form_submit_button(label='Send')
 
         if submit_button and user_input:
-            with st.spinner('Generating response...'):
+            with st.spinner(' Looking into the files...'):
                 output = conversation_chat(user_input, chain, st.session_state['history'])
 
             st.session_state['past'].append(user_input)
@@ -51,11 +59,17 @@ def display_chat_history(chain):
                 message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
 def create_conversational_chain(vector_store):
+    load_dotenv()
+    # Create llm
+    #llm = CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",
+                        #streaming=True, 
+                        #callbacks=[StreamingStdOutCallbackHandler()],
+                        #model_type="llama", config={'max_new_tokens': 500, 'temperature': 0.01})
     llm = Replicate(
-        streaming=True,
-        model="replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781", 
+        streaming = True,
+        model = "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781", 
         callbacks=[StreamingStdOutCallbackHandler()],
-        input={"temperature": 0.01, "max_length": 500, "top_p": 1})
+        input = {"temperature": 0.01, "max_length" :500,"top_p":1})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
@@ -63,73 +77,55 @@ def create_conversational_chain(vector_store):
                                                  memory=memory)
     return chain
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def process_uploaded_files(uploaded_files):
-    text = []
-    for file in uploaded_files:
-        try:
+def main():
+    load_dotenv()
+    # Initialize session state
+    initialize_session_state()
+    st.title("Evaluation tool for contracts sustainability ")
+    st.write('This project was Developed by group 1 as showcase for 602 - Project procurement course ')
+    # Initialize Streamlit
+    st.sidebar.title("Please drop your contract documents here ")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
+
+    image = Image.open('./logo/alberta-logo-university-com.png')
+    st.sidebar.image(image, use_column_width=True)
+
+    image = Image.open('./logo/Faculty_Wordmark_Standard.png')
+    st.sidebar.image(image, use_column_width=True)
+    
+    if uploaded_files:
+        text = []
+        for file in uploaded_files:
             file_extension = os.path.splitext(file.name)[1]
-            with tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) as temp_file:
-                temp_file.write(file.getvalue())
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
                 temp_file_path = temp_file.name
 
             loader = None
             if file_extension == ".pdf":
                 loader = PyPDFLoader(temp_file_path)
-            elif file_extension in [".docx", ".doc"]:
+            elif file_extension == ".docx" or file_extension == ".doc":
                 loader = Docx2txtLoader(temp_file_path)
             elif file_extension == ".txt":
                 loader = TextLoader(temp_file_path)
 
             if loader:
-                loaded_text = loader.load()
-                text.extend(loaded_text)
+                text.extend(loader.load())
                 os.remove(temp_file_path)
 
-            # Debugging: Print or log the extracted text
-            print(f"Loaded text from {file.name}: {loaded_text[:500]}")  # Print first 500 characters
-
-        except Exception as e:
-            st.error(f"Error processing file {file.name}: {e}")
-            continue
-    return text
-
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def load_embeddings(text_chunks):
-    # Ensure each chunk is a string and replace newlines with spaces
-    cleaned_text_chunks = [chunk.replace("\n", " ") if isinstance(chunk, str) else "" for chunk in text_chunks]
-
-    # Debugging: Print or log the cleaned text chunks
-    print(f"Cleaned text chunks: {cleaned_text_chunks[:3]}")  # Print first 3 chunks
-
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
-                                       model_kwargs={'device': 'cpu'})
-    return embeddings.embed_documents(cleaned_text_chunks)
-
-
-def main():
-    load_dotenv()
-    initialize_session_state()
-    st.title("Evaluation tool for contracts sustainability evaluation ")
-    st.sidebar.title("Drop your contract documents here ")
-    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
-
-    # Debugging: Inspect the uploaded_files variable
-    print(f"Uploaded files: {uploaded_files}")
-
-if uploaded_files:
-        text = process_uploaded_files(uploaded_files)
         text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len)
         text_chunks = text_splitter.split_documents(text)
-        embeddings = load_embeddings(text_chunks)
 
-        # Debugging: Inspect embeddings structure
-        print(f"Embeddings: {embeddings[:3]}")  # Print first 3 embeddings
+        # Create embeddings
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
+                                           model_kwargs={'device': 'cpu'})
 
-        vector_store = FAISS.from_documents(embeddings, embedding=None)
+        # Create vector store
+        vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
 
         # Create the chain object
         chain = create_conversational_chain(vector_store)
+
         
         display_chat_history(chain)
 
