3e79e5d7016b8e9766e71e7f44dabdbaeac20f64
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2023-11-13 01:49:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -82,11 +82,11 @@ def load_embeddings(text_chunks):
 
     loader = None
     if file_extension == ".pdf":
-    loader = PyPDFLoader(temp_file_path)
+        loader = PyPDFLoader(temp_file_path)
     elif file_extension in [".docx", ".doc"]:
-    loader = Docx2txtLoader(temp_file_path)
+        loader = Docx2txtLoader(temp_file_path)
     elif file_extension == ".txt":
-    loader = TextLoader(temp_file_path)
+        loader = TextLoader(temp_file_path)
 
         if loader:
         text.extend(loader.load())
