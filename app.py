72f8663fc60d39220929368ffbf915ad393ca938
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2023-11-13 01:44:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from langchain.llms import CTransformers, Replicate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import os
 from dotenv import load_dotenv
 import tempfile
