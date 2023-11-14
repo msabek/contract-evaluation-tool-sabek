845da72cbced982be539ff04bd0e639579b04ef7
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2023-11-13 20:59:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from PIL import Image
 from streamlit_chat import message
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
 from langchain.llms import Replicate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import Docx2txtLoader
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import os
+from dotenv import load_dotenv
 import tempfile
 
 # Load environment variables
